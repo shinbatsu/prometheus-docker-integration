f42cc86e8e46bd016fdd7e14963209d3623a429f
shinbatsu <nojic132@gmail.com>
2023-01-18 19:59:21 +0300

    implement metrics collector

--- main.go
+++ main.go
@@ -40,3 +40,53 @@ var (
 
 	docker *client.Client
 )
+type dockerCollector struct{}
+
+func (c dockerCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- memUsageDesc
+	ch <- memReservationDesc
+	ch <- memLimitDesc
+}
+
+func (c dockerCollector) Collect(ch chan<- prometheus.Metric) {
+	cgroupVersion := detectCgroupVersion()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	containers, err := docker.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		log.Printf("failed to list containers: %v", err)
+		return
+	}
+
+	for _, container := range containers {
+		name := cleanContainerName(container.Names)
+		stack, service := extractStackService(container.Labels)
+
+		usageBytes, err := containerUsageBytes(container.ID, cgroupVersion)
+		if err != nil {
+			log.Printf("failed to read usage for container %s: %v", name, err)
+			continue
+		}
+
+		totalCache, err := containerTotalCacheBytes(container.ID, cgroupVersion)
+		if err != nil {
+			log.Printf("failed to read cache for container %s: %v", name, err)
+			continue
+		}
+
+		inspect, err := docker.ContainerInspect(ctx, container.ID)
+		if err != nil {
+			log.Printf("failed to inspect container %s: %v", name, err)
+			continue
+		}
+
+		emitMemoryMetrics(ch, name, stack, service, usageBytes-totalCache, inspect.HostConfig.MemoryReservation, inspect.HostConfig.Memory)
+	}
+}
+
+func emitMemoryMetrics(ch chan<- prometheus.Metric, name, stack, service string, usage, reservation, limit int64) {
+	ch <- prometheus.MustNewConstMetric(memUsageDesc, prometheus.GaugeValue, float64(usage), name, stack, service)
+	ch <- prometheus.MustNewConstMetric(memReservationDesc, prometheus.GaugeValue, float64(reservation), name, stack, service)
+	ch <- prometheus.MustNewConstMetric(memLimitDesc, prometheus.GaugeValue, float64(limit), name, stack, service)
+}
